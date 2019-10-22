0565f8dd128aa0c10a82caf6a48e64935f5d63ee
Tzu Hsiang Lin <t9360341@ntut.org.tw>
2019-10-23 00:24:54 +0800

    Add more comments.

--- algorithm/Sort/15.py
+++ algorithm/Sort/15.py
@@ -22,25 +22,35 @@ from typing import *
 
 class Solution:
     def threeSum(self, nums: List[int]) -> List[List[int]]:
+        # Sorted take nlogn here since brute foce take n*n*n
         nums = sorted(nums)
+        
+        # NG Case: Length OK(>= 3) but all zeros
         if (len(nums) >= 3) and (nums[0] == nums[len(nums) -1]) and (nums[0] == 0):
             return [[0, 0, 0]]
-
+        
         triplets = []
+        # 0~lenght-2, two for left and right pointers
         for index in range(len(nums) - 2):
-            left = index+1
+            left = index + 1
             right = len(nums) - 1
+            # two pointers sinces sorted
             while left < right:
                 sum = nums[index] + nums[left] + nums[right]
                 if sum == 0:
-                    triplets.append((nums[index],nums[left],nums[right])) #Use tuple here, unhashable type: 'list'
+                    #Use tuple here, since 'list' is unhashable!!
+                    triplets.append((nums[index],nums[left],nums[right]))
                     left += 1
                     right -= 1
                 elif sum < 0:
+                    # Move left+1 to sum larger
                     left += 1
                 else:
+                    # Move right-1 to sum smaller
                     right -= 1
-        return list(set(triplets)) #Use set to filter out duplicate
+        # Use set to filter out duplicate
+        # All duplicates are same elements since sorted
+        return list(set(triplets))
 
 print(Solution().threeSum([-1, 0, 1, 2, -1, -4]))
 #[  [-1, 0, 1],[-1, -1, 2] ]
