0aed85da9c92b2e883f6fc7cb75031d7813129e2
Tzu Hsiang Lin <t9360341@ntut.org.tw>
2019-11-05 02:05:30 +0800

    Add more comments and refactoring.

--- algorithm/Tree/863.py
+++ algorithm/Tree/863.py
@@ -1,21 +1,39 @@
 '''
 863. All Nodes Distance K in Binary Tree
 https://leetcode.com/problems/all-nodes-distance-k-in-binary-tree
-Input: root = [3,5,1,6,2,0,8,null,null,7,4], target = 5, K = 2
 
+We are given a binary tree (with root node root),
+a target node, and an integer value K.
+Return a list of the values of all nodes that 
+have a distance K from the target node.  
+The answer can be returned in any order.
+
+Input: 
+ root(TreeNodes) = [3,5,1,6,2,0,8,null,null,7,4], 
+ target(TreeNodes) = 5, K(int) = 2
 Output: [7,4,1]
+Note that the inputs "root" and "target" are actually TreeNodes.
+The descriptions of the inputs above 
+are just serializations of these objects.
 
 Explanation: 
-The nodes that are a distance 2 from the target node (with value 5)
+The nodes that are a distance 2 from 
+the target node (with value 5)
 have values 7, 4, and 1.
 
-Hi, here's your problem today. This problem was recently asked by Amazon:
-Given a binary tree, return all values given a certain height h.
-#     1
-#    / \
-#   2   3
-#  / \   \
-# 4   5   7
+#     3
+#    /  \
+#   5    1
+#  / \  / \
+#  6 2  0 8
+#   / \
+#   7 4
+
+Note:
+The given tree is non-empty.
+Each node in the tree has unique values 0 <= node.val <= 500.
+The target node is a node in the tree.
+0 <= K <= 1000.
 '''
 # Definition for a binary tree node.
 class Node:
@@ -42,7 +60,7 @@ class Solution:
         result = []
         self.addParentField(root)
         if root and target:
-            q= collections.deque()
+            q  = collections.deque()
             q.append((target, 0))
             seen = { target }
             while q:
