0837f3011f759cd628735b3076225c1ba3a10f99
Tzu Hsiang Lin <t9360341@ntut.org.tw>
2019-11-05 23:28:02 +0800

    Add more comments and refactoring.

--- algorithm/Sort/Sorting.py
+++ algorithm/Sort/Sorting.py
@@ -1,50 +1,69 @@
 def insertion_sort(A):
-    for key_index in range(1,len(A)):#1...len(A)-1
-        key_value = A[key_index]
-        compare_index = key_index-1
-        while compare_index>=0 and A[compare_index]>key_value:
-            A[compare_index+1]=A[compare_index]
-            compare_index -= 1
-        A[compare_index+1] = key_value
+    for idx in range(1,len(A)):#1...len(A)-1
+        key = A[idx]
+        cmp_idx = idx-1
+        while cmp_idx>=0 and A[cmp_idx]>key:
+            A[cmp_idx+1]=A[cmp_idx]
+            cmp_idx -= 1
+        A[cmp_idx+1] = key
+
+def partition(A, start, end):
+    pivot_idx = end-1
+    mid_idx = start
+    for cmp_idx in range(start, pivot_idx):
+        if A[cmp_idx] <= A[pivot_idx]:
+            A[mid_idx],A[cmp_idx] = A[cmp_idx],A[mid_idx]
+            mid_idx += 1
+    A[mid_idx],A[pivot_idx] = A[pivot_idx],A[mid_idx]
+    return mid_idx
+
+def quick_sort(A, start=0, end=None):
+    if end is None:
+        end = len(A)
+    if start < end:
+        mid = partition(A, start, end)
+        quick_sort(A, start, mid)#start...mid-1
+        quick_sort(A, mid+1, end)#mid+1...end-1
 
 def merge(left, right):
     result = []
-    left_index = 0
-    right_index = 0
-    while left_index < len(left) and right_index<len(right):
-        if left[left_index] < right[right_index]:
-            result.append(left[left_index])
-            left_index += 1
+    left_idx = 0
+    right_idx = 0
+    while left_idx < len(left) and right_idx < len(right):
+        if left[left_idx] < right[right_idx]:
+            result.append(left[left_idx])
+            left_idx += 1
         else:
-            result.append(right[right_index])
-            right_index += 1
-    result.extend(left[left_index:len(left)])
-    result.extend(right[right_index:len(right)])
+            result.append(right[right_idx])
+            right_idx += 1
+    #Copy remaining elements...
+    result.extend(left[left_idx:])
+    result.extend(right[right_idx:])
     return result
 
 def merge_sort(A):
     if len(A)<=1:
         return A
     mid = len(A) // 2
-    left = merge_sort(A[0:mid])
-    right = merge_sort(A[mid:len(A)])
+    left = merge_sort(A[:mid])
+    right = merge_sort(A[mid:])
     return merge(left, right)
 
-def max_heapify(A, root_index, heap_size):
-    left_index = 2*root_index + 1
-    right_index = 2*root_index + 2
-    max_index = root_index
-    if left_index < heap_size and A[left_index]>A[max_index]:
-        max_index = left_index
-    if right_index < heap_size and A[right_index]>A[max_index]:
-        max_index = right_index
-    if max_index != root_index:
-        A[root_index], A[max_index] = A[max_index],A[root_index]
-        max_heapify(A, max_index, heap_size)
+def max_heapify(A, root_idx, heap_size):
+    left_idx = 2*root_idx + 1
+    right_idx = 2*root_idx + 2
+    max_idx = root_idx
+    if left_idx < heap_size and A[left_idx]>A[max_idx]:
+        max_idx = left_idx
+    if right_idx < heap_size and A[right_idx]>A[max_idx]:
+        max_idx = right_idx
+    if max_idx != root_idx:
+        A[root_idx], A[max_idx] = A[max_idx],A[root_idx]
+        max_heapify(A, max_idx, heap_size)
 
 def build_max_heap(A):
-    for index in range(len(A)//2,-1,-1):#len(A)//2...0
-        max_heapify(A, index, len(A))
+    for idx in range(len(A)//2,-1,-1):#len(A)//2...0
+        max_heapify(A, idx, len(A))
 
 def my_heap_sort(A):
     build_max_heap(A)
@@ -57,24 +76,6 @@ def my_heap_sort(A):
 #    for value in iterable:
 #        heapq.heappush(h, value)
 #    return [heapq.heappop(h) for i in range(len(h))]
-
-def partition(A, start, end):
-    pivot_index = end-1
-    mid_index = start
-    for large_index in range(start, pivot_index):
-        if A[large_index] <= A[pivot_index]:
-            A[mid_index],A[large_index] = A[large_index],A[mid_index]
-            mid_index += 1
-    A[mid_index],A[pivot_index] = A[pivot_index],A[mid_index]
-    return mid_index
-
-def quick_sort(A, start=0, end=None):
-    if end is None:
-        end = len(A)
-    if start < end:
-        mid = partition(A, start, end)
-        quick_sort(A, start, mid)#start...mid-1
-        quick_sort(A, mid+1, end)#mid+1...end-1
 
 def counting_sort(str):
     counter = [ 0 for i in range(26) ]
@@ -103,10 +104,10 @@ if __name__ == "__main__":
     #print("merge_sort: {}".format(merge_sort(A)))
     #print("After merge_sort: {}".format(A))
 
-    #my_heap_sort(A)
-    #print("After my_heap_sort: {}".format(A))
+    my_heap_sort(A)
+    print("After my_heap_sort: {}".format(A))
 
-    quick_sort(A)
-    print("After quick_sort: {}".format(A))
+    #quick_sort(A)
+    #print("After quick_sort: {}".format(A))
     
-    print("counting_sort: {}".format(counting_sort("traceback")))
+    #print("counting_sort: {}".format(counting_sort("traceback")))
